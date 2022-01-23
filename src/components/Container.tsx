89dcd66e7c82f52eb151f16dec75fe35a2a97056
Massimiliano Aprea <maprea@keybiz.it>
2022-01-23 17:03:44 +0100

    feat: add responsive configuration to Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,10 +2,30 @@ import styled from 'styled-components';
 import theme from '../theme';
 
 const Container = styled.div`
-  max-width: 960px;
   margin-top: ${theme.spacing(6)};
   margin-left: auto;
   margin-right: auto;
+
+  @media (max-width: ${theme.breakpoints.xs.viewport}px) {
+    max-width: 100%;
+    padding: 0 ${theme.spacing(3)};
+  }
+
+  @media (min-width: ${theme.breakpoints.xs.viewport}px) and (max-width: ${theme
+      .breakpoints.md.viewport}px) {
+    max-width: ${theme.breakpoints.md.container}px;
+    padding: 0 ${theme.spacing(3)};
+  }
+
+  @media (min-width: ${theme.breakpoints.md.viewport}px) and (max-width: ${theme
+      .breakpoints.lg.viewport}px) {
+    max-width: ${theme.breakpoints.lg.container}px;
+    padding: 0 ${theme.spacing(3)};
+  }
+
+  @media (min-width: ${theme.breakpoints.lg.viewport}px) {
+    max-width: ${theme.breakpoints.lg.container}px;
+  }
 `;
 
 export default Container;
