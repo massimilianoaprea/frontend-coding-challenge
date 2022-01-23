5b0d0292e834bf2992f4387e8278cb27f9be3028
Massimiliano Aprea <maprea@keybiz.it>
2022-01-23 16:50:49 +0100

    chore: add translation file

---
+++ src/i18/languages.ts
@@ -0,0 +1,46 @@
+export const TRANSLATIONS = {
+  en: {
+    dateFormat: '{{date, DD/MM/YYYY, HH:mm:ss}}',
+    tournament: {
+      search: 'Search tournament...',
+      create: 'Create tournament',
+      loading: 'Loading Tournaments...',
+      error: 'Something went wrong.',
+      found: 'Tournaments found',
+      notFound: 'No tournaments found.',
+      name: 'Tournament Name'
+    },
+    retry: 'Retry',
+    organizer: 'Organizer',
+    game: 'Game',
+    participants: 'Participants',
+    start: 'Start',
+    edit: 'Edit',
+    delete: 'Delete',
+    newTournamentName: 'New Tournament Name',
+    noNameError: "The Tournament name can't be empty!",
+    areYouSure: 'Do you really want to delete this tournament?'
+  },
+  it: {
+    dateFormat: '{{date, DD/MM/YYYY, HH:mm:ss}}',
+    tournament: {
+      search: 'Ricerca torneo...',
+      create: 'Crea torneo',
+      loading: 'Sto caricando i tornei...',
+      error: 'Qualcosa è andato storto.',
+      found: 'Tornei trovati',
+      notFound: 'Nessun torneo trovato.',
+      name: 'Nome Torneo'
+    },
+    retry: 'Riprova',
+    organizer: 'Organizzatore',
+    game: 'Gioco',
+    participants: 'Partecipanti',
+    start: 'Inizio',
+    edit: 'Modifica',
+    delete: 'Elimina',
+    newTournamentName: 'Nuovo Nome del Torneo',
+    noNameError: 'Il nome del torneo non può essere vuoto!',
+    areYouSure: 'Sei davvero sicuro di voler eliminare il torneo?'
+  }
+};
